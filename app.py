2ef0b9a0c5585d4d38576dbc723d7a72a7f8bce8
Ejaram32 <teban.jar@gmail.com>
2026-06-08 21:31:38 -0500

    se adelantasobre la publicacion en streamlit

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
 import pandas as pd
 import streamlit as st
-import plotly as pl
+import plotly.graph_objects as go
+
+vehicles = pd.read_csv('vehicles_us.csv')
+
+st.header("Analisis de datos ventas de vehiculos")
+
+
+
+hist_button = st.button('construir histograma')
+disp_button = st.button('construir grafico de dispersion')
+
+if hist_button:
+    st.write("construccion del histograma para el conjunto de datos")
+    fig = go.Figure(data = [go.Histogram(x = vehicles['odometer'])])
+    fig.update_layout(title_text = 'Distribucion del Odometro')
+    st.plotly_chart(fig, use_container_width=True)
+
+if disp_button:
+    st.write("construccion del grafico de dispersion para el conjunto de datos")
+    fig = go.Figure(data = [go.Scatter(x = vehicles['odometer'], y = vehicles['price'], mode ='markers')])
+    fig.update_layout(title_text = 'Relacion entre Odometro y Precio')
+    st.plotly_chart(fig, use_container_width=True)
